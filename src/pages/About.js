9f4d4e9f9d1b5d968cfe21269518cb7d913f6753
bencha27 <115957643+bencha27@users.noreply.github.com>
2023-04-28 12:33:18 -0600

    Updated Skills section of About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,25 @@ import "./About.css";
 import alliantShield from "../images/Alliant-International-University-Shield.png";
 import berkeleyShield from "../images/University-of-California-Berkeley-Shield.png";
 import denverShield from "../images/University-of-Denver-Shield.png";
+import htmlLogo from "../images/HTML5-Logo.png";
+import cssLogo from "../images/CSS3-Logo.png";
+import jsLogo from "../images/JavaScript-ES6-Logo.jpg";
+import bootstrapLogo from "../images/Bootstrap-Logo.png";
+import jqueryLogo from "../images/jQuery-Logo.png";
+import handlebarsLogo from "../images/Handlebars-Logo.png";
+import pwaLogo from "../images/PWA-Logo.png";
+import webpackLogo from "../images/Webpack-Logo.png";
+import reactLogo from "../images/React-Logo.png";
+import nodeLogo from "../images/Node-Logo.png";
+import jestLogo from "../images/Jest-Logo.png";
+import oopLogo from "../images/OOP-Logo.png";
+import expressLogo from "../images/Express-Logo.png";
+import mysqlLogo from "../images/MySQL-Logo.png";
+import mvcLogo from "../images/MVC-Logo.png";
+import mongodbLogo from "../images/MongoDB-Logo.png";
+import graphqlLogo from "../images/GraphQL-Logo.png";
+import mernLogo from "../images/MERN-Stack-Logo.png";
+import restapiLogo from "../images/REST-API-Logo.png";
 
 export default function About() {
   return (
@@ -30,33 +49,15 @@ export default function About() {
           <h3 className="about-section-heading">Education</h3>
             <ul>
               <li>
-                <img
-                  src={denverShield}
-                  alt="University of Denver Shield"
-                  width="20"
-                  height="20"
-                  style={{marginRight: 5}}
-                ></img>
+                <img src={denverShield} alt="University of Denver Shield"></img>
                 Full Stack Coding Boot Camp • University of Denver
               </li>
               <li>
-                <img
-                  src={alliantShield}
-                  alt="Alliant International University Shield"
-                  width="20"
-                  height="20"
-                  style={{marginRight: 5}}
-                ></img>
+                <img src={alliantShield} alt="Alliant International University Shield"></img>
                 M.A. Education • Alliant International University
               </li>
               <li>
-                <img
-                  src={berkeleyShield}
-                  alt="University of California, Berkeley Shield"
-                  width="20"
-                  height="20"
-                  style={{marginRight: 5}}
-                ></img>
+                <img src={berkeleyShield} alt="University of California, Berkeley Shield"></img>
                 B.A. Political Economy • University of California, Berkeley
               </li>
             </ul>
@@ -77,29 +78,30 @@ export default function About() {
         {/* Skills */}
         <div className="skills-section col-3">
           <h3 className="skills-section-heading">Skills</h3>
-          <h4 className="skills-section-subheading">Front-End</h4>
+          <h5 className="skills-section-subheading">Front-End</h5>
           <ul>
-            <li>HTML5</li>
-            <li>CSS3</li>
-            <li>JavaScript ES6</li>
-            <li>Bootstrap</li>
-            <li>jQuery</li>
-            <li>AJAX</li>
-            <li>Handlebars</li>
-            <li>PWA</li>
-            <li>Webpack</li>
-            <li>React</li>
+            <li><img src={htmlLogo} alt="HTML5 Logo"></img>HTML5</li>
+            <li><img src={cssLogo} alt="CSS3 Logo"></img>CSS3</li>
+            <li><img src={jsLogo} alt="JS ES6 Logo"></img>JavaScript ES6</li>
+            <li><img src={jqueryLogo} alt="jQuery Logo"></img>jQuery</li>
+            <li><img src={bootstrapLogo} alt="Bootstrap Logo"></img>Bootstrap</li>
+            <li><img src={reactLogo} alt="React Logo"></img>React</li>
+            <li><img src={webpackLogo} alt="Webpack Logo"></img>Webpack</li>
+            <li><img src={handlebarsLogo} alt="Handlebars Logo"></img>Handlebars</li>
+            <li><img src={pwaLogo} alt="PWA Logo"></img>Progressive Web Apps</li>
           </ul>
-          <h4 className="skills-section-subheading">Back-End</h4>
+          <h5 className="skills-section-subheading">Back-End</h5>
           <ul>
-            <li>Node</li>
-            <li>Jest</li>
-            <li>OOP (JS)</li>
-            <li>Express</li>
-            <li>MySQL</li>
-            <li>MongoDB</li>
-            <li>GraphQL</li>
-            <li>MERN</li>
+            <li><img src={nodeLogo} alt="Node Logo"></img>Node</li>
+            <li><img src={expressLogo} alt="Express Logo"></img>Express</li>
+            <li><img src={restapiLogo} alt="REST API Logo"></img>REST API</li>
+            <li><img src={mysqlLogo} alt="MySQL Logo"></img>MySQL</li>
+            <li><img src={mongodbLogo} alt="MongoDB Logo"></img>MongoDB</li>
+            <li><img src={graphqlLogo} alt="GraphQL Logo"></img>GraphQL</li>
+            <li><img src={mernLogo} alt="MERN Logo"></img>MERN Stack</li>
+            <li><img src={jestLogo} alt="Jest Logo"></img>Jest</li>
+            <li><img src={oopLogo} alt="OOP Logo"></img>Object-Oriented Programming</li>
+            <li><img src={mvcLogo} alt="MVC Logo"></img>Model-View-Controller Architecture</li>
           </ul>
         </div>
       </div>
