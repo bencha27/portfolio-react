c3d30e805e580c8bcd5ac9365b6361dafadf058e
Benjamin Cha <mrbrenjamincha@gmail.com>
2024-08-15 13:23:04 -0600

    Update routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 // Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from './pages/Home';
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-// import Error from './pages/Error';
 
 export default function App() {
   return (
-    <Router>
+    <>
       <Header />
-      <Routes>
-        <Route path="/portfolio/" element={<Home />} />
-        <Route path="/portfolio/about" element={<About />} />
-        <Route path="/portfolio/projects" element={<Projects />} />
-        <Route path="/portfolio/contact" element={<Contact />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <Outlet />
       <Footer />
-    </Router>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
-
 import App from './App';
+import Home from './pages/Home';
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "portfolio",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      }
+    ]
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
